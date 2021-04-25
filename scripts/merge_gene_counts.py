c45b9d9f9556db721cf8019ddb99200317f622bf
Muhammed Hasan Celik <muhammedhasancelik@gmail.com>
2021-04-25 04:13:12 +0200

    aberrant genes

--- scripts/merge_gene_counts.py
+++ scripts/merge_gene_counts.py
@@ -1,18 +1,18 @@
+import pdb
 from pathlib import Path
 import pandas as pd
 
 
-samples = pd.read_csv(snakemake.input['samples'])['Participant_ID'].tolist()
 count_dir = Path(snakemake.input['count_dir'])
+samples = pd.read_csv(snakemake.input['samples'])['Participant_ID'].tolist()
+samples = set(samples).intersection(i.name for i in list(count_dir.iterdir()))
 
 dfs = list()
 
 for i in samples:
     path = next((count_dir / i).rglob('*.txt'))
-
-    df = pd.read_csv(path, sep='\t', index_col=0)
+    df = pd.read_csv(path, sep='\t', comment='#', index_col=0)
     dfs.append(df[[df.columns[-1]]])
-
 
 df = pd.concat(dfs, axis=1)
 
