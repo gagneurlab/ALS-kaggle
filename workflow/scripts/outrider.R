fd0ccf50853b70dd92f32ae1d7e571ffd7a9df3a
Michaela Müller <mumichae@in.tum.de>
2021-04-28 01:48:30 +0200

    fixed missing filter before running outrider

--- workflow/scripts/outrider.R
+++ workflow/scripts/outrider.R
@@ -5,9 +5,15 @@ suppressPackageStartupMessages({
 padj_cutoff <- snakemake@params$padj_cutoff
 
 ods <- readRDS(snakemake@input$ods)
-ods <- OUTRIDER(ods)
-res <- results(ods, padjCutoff = padj_cutoff)
 
+# apply filter
+ods <- ods[mcols(ods)$passedFilter,] 
+
+# run end-to-end pipeline
+ods <- OUTRIDER(ods)
 saveRDS(ods, snakemake@output$ods)
+
+# extract and save results
+res <- results(ods, padjCutoff = padj_cutoff)
 fwrite(res, snakemake@output$results)
 
