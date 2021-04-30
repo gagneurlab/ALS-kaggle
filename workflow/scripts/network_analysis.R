69785e8f3fd4a071bb4013f4c30d36b6025e8c5a
Michaela Müller <mumichae@in.tum.de>
2021-04-30 17:54:53 +0200

    added plots for exp/obs counts, seed probablility distribution

--- workflow/scripts/network_analysis.R
+++ workflow/scripts/network_analysis.R
@@ -1,5 +1,6 @@
 suppressPackageStartupMessages({
     library(data.table)
+    library(ggplot2)
 })
 
 
@@ -15,7 +16,25 @@ outliers <- merge(
     all.x = TRUE
 )
 
-gene_seed_prob <- gene_seed_prob[gene_id %in% outliers$geneID]
+gene_seed_prob_sub <- gene_seed_prob[gene_id %in% outliers$geneID]
 
 fwrite(outliers, snakemake@output$outliers)
-fwrite(gene_seed_prob, snakemake@output$gene_seed_prob_sub)
+fwrite(gene_seed_prob_sub, snakemake@output$gene_seed_prob_sub)
+
+dt_gene_prob <- rbind(
+    gene_seed_prob[, .(score, gene_set = 'genome')],
+    gene_seed_prob_sub[, .(score, gene_set = 'expression outlier')]
+)
+dt_gene_prob[
+    , gene_set := factor(
+        gene_set,
+        levels = c('genome', 'expression outlier'))
+    ]
+
+ggplot(dt_gene_prob, aes(-log10(score), fill = gene_set)) + 
+    geom_histogram(position = 'identity', bins = 100, alpha = 0.8) +
+    theme_classic() +
+    theme(legend.position = 'bottom') +
+    scale_fill_brewer(palette = 'Set1')
+
+ggsave(snakemake@output$histogram)
--- workflow/scripts/outrider_plots.R
+++ workflow/scripts/outrider_plots.R
@@ -35,3 +35,29 @@ ggsave(snakemake@output$expr_genes)
 res <- results(ods, all = TRUE)
 ggplot(res, aes(pValue)) + geom_histogram(bins = 100) + theme_classic()
 ggsave(snakemake@output$pvalues)
+
+cnts <- data.table(
+    observed = as.vector(counts(ods)),
+    expected = as.vector(normalizationFactors(ods)),
+    aberrant = as.vector(aberrant(ods))
+)
+ggplot(cnts, aes(log10(expected + 1), log10(observed + 1), color = aberrant)) +
+    geom_point() +
+    scale_color_brewer(palette = 'Set1') +
+    theme_classic() +
+    theme(legend.position = 'bottom')
+ggsave(snakemake@output$exp_obs_counts)
+
+# cnts_long <- melt(
+#     cnts,
+#     measure.vars = c('expected', 'observed'),
+#     variable.name = 'count_type',
+#     value.name = 'counts'
+# )
+# 
+# ggplot(cnts_long, aes(log10(counts + 1), after_stat(count), color = count_type)) +
+#     geom_density(position = 'identity') +
+#     scale_color_brewer(palette = 'Set1') +
+#     theme_classic() +
+#     theme(legend.position = 'bottom')
+# 
