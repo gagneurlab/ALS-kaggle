ed4e87ec7eb89d484c1f6f15a9e2be8a2426acc7
Muhammed Hasan Celik <muhammedhasancelik@gmail.com>
2021-05-08 09:20:24 +0200

    umap

--- workflow/scripts/umap.py
+++ workflow/scripts/umap.py
@@ -1,17 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import umap
-import umap.plot
 import numba
+import seaborn as sns
+sns.set_theme(style="whitegrid")
+
 
 numba.set_num_threads(snakemake.threads)
 
-gene_counts = pd.read_table(snakemake.input['counts'], sep = '\t')
-# TODO: add sample annotations
+gene_counts = pd.read_table(snakemake.input['counts'], sep='\t')
 
 del gene_counts['gene_id']
+
 X = gene_counts.transpose()
 embed_counts = umap.UMAP().fit(X)
 
-umap.plot.points(embed_counts)
-plt.savefig(snakemake.output['plot'])
+df_samples = pd.read_csv(snakemake.input['samples'], sep='\t')
+df_samples = df_samples.set_index('Participant_ID').loc[gene_counts.columns]
+df_samples['dim1'] = embed_counts.embedding_[:, 0]
+df_samples['dim2'] = embed_counts.embedding_[:, 1]
+
+plt.figure(figsize=(5, 5), dpi=250)
+sns.scatterplot(data=df_samples, x="dim1", y="dim2", hue='Case_Control')
+plt.savefig(snakemake.output['plot_case_control'])
+plt.clf()
+
+plt.figure(figsize=(5, 5), dpi=250)
+df = df_samples[~df_samples['Site_of_Onset'].isna()]
+sns.scatterplot(data=df, x="dim1", y="dim2", hue='Site_of_Onset')
+plt.savefig(snakemake.output['plot_bulbar_vs_limb'])
