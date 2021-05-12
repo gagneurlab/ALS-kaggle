5681e6094a5988dabcf607ea574b4e8737fa16da
Michaela Müller <mumichae@in.tum.de>
2021-05-12 15:29:42 +0200

    updated OUTRIDER plots, add BCV

--- workflow/scripts/outrider_plots.R
+++ workflow/scripts/outrider_plots.R
@@ -6,7 +6,7 @@ suppressPackageStartupMessages({
 
 anno_groups <- snakemake@params$anno_groups
 ods <- readRDS(snakemake@input$ods)
-sample_annotation <- fread(snakemake@input$sample_annotation)
+sample_annotation <- fread(snakemake@input$sample_annotation, na.strings = c('NA', ''))
 sample_annotation <- sample_annotation[, c('Participant_ID', anno_groups), with = FALSE]
 sample_annotation[, Participant_ID := make.names(Participant_ID)]
 
@@ -25,18 +25,6 @@ png(snakemake@output$power)
 plotPowerAnalysis(ods)
 dev.off()
 
-png(snakemake@output$heatmap_raw, width = 1000, height = 1000)
-plotCountCorHeatmap(ods, normalized = FALSE, colGroups = anno_groups, colColSet = "Set1")
-dev.off()
-
-png(snakemake@output$heatmap_fit, width = 1000, height = 1000)
-plotCountCorHeatmap(ods, normalized = TRUE, colGroups = anno_groups, colColSet = "Set1")
-dev.off()
-
-png(snakemake@output$gene_vs_sample, width = 1000, height = 1000)
-plotCountGeneSampleHeatmap(ods, normalized = TRUE, nGenes = 1000, colGroups = anno_groups, colColSet = "Set1")
-dev.off()
-
 png(snakemake@output$per_sample)
 plotAberrantPerSample(ods)
 dev.off()
@@ -44,7 +32,6 @@ dev.off()
 png(snakemake@output$qqplot)
 plotQQ(ods, global = TRUE)
 dev.off()
-
 
 plotEncDimSearch(ods) + theme_bw()
 ggsave(snakemake@output$dim_search)
@@ -81,3 +68,74 @@ ggsave(snakemake@output$exp_obs_counts)
 #     theme_classic() +
 #     theme(legend.position = 'bottom')
 # 
+
+
+# BCV
+estimateThetaWithoutAutoCorrect <- function(ods){
+    ods1 <- OutriderDataSet(countData=counts(ods), colData=colData(ods))
+    # use rowMeans as expected means
+    normalizationFactors(ods1) <- matrix(
+        rowMeans(counts(ods1)),
+        ncol=ncol(ods1),
+        nrow=nrow(ods1)
+    )
+    ods1 <- fit(ods1)
+    theta(ods1)
+}
+
+before <- data.table(
+    autocorrect = "No",
+    BCV = 1/sqrt(estimateThetaWithoutAutoCorrect(ods))
+)
+after <- data.table(
+    autocorrect = "Yes",
+    BCV = 1/sqrt(theta(ods))
+)
+bcv_dt <- cbind(geneID = rownames(ods), rbind(before, after))
+
+ggplot(bcv_dt, aes(autocorrect, BCV)) +
+    geom_boxplot() +
+    theme_bw(base_size = 14) +
+    labs(
+        x = "Autoencoder corrected",
+        y = "BCV",
+        title = "Biological coefficient of variation"
+    )
+ggsave(snakemake@output$bcv)
+
+fwrite(
+    bcv_dt[autocorrect == 'Yes'][order(BCV, decreasing = TRUE)],
+    snakemake@output$bcv_dt
+)
+
+
+# Heatmaps
+dev.off()
+
+png(snakemake@output$gene_vs_sample, width = 1000, height = 1000)
+plotCountGeneSampleHeatmap(
+    ods,
+    normalized = FALSE,
+    nGenes = 50,
+    colGroups = anno_groups,
+    main = 'Gene counts vs Sample Heatmap'
+)
+dev.off()
+
+png(snakemake@output$heatmap_raw, width = 1000, height = 1000)
+plotCountCorHeatmap(
+    ods,
+    normalized = FALSE,
+    colGroups = anno_groups,
+    main = 'Raw Count Correlation'
+)
+dev.off()
+
+png(snakemake@output$heatmap_fit, width = 1000, height = 1000)
+plotCountCorHeatmap(
+    ods,
+    normalized = TRUE,
+    colGroups = anno_groups,
+    main = 'Normalized Count Correlation'
+)
+dev.off()
